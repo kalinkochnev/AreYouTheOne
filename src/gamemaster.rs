46dfff5188e9116605309d6847c10041d5612cfa
kalinkochnev <kalin.kochnev@gmail.com>
2022-04-04 17:12:01 -0400

    Added game strategy interface

--- src/gamemaster.rs
+++ src/gamemaster.rs
@@ -1,67 +1,100 @@
-use crate::contestant::{Player, ContestantPair, Players};
+use crate::contestant::{ContestantPair, Player};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-
 pub struct GameMaster {
     iterations: i32,
-    pairs: Vec<ContestantPair>,
+    num_matched: i32,
+    matches: Vec<ContestantPair>,
+    max_iterations: i32,
 }
 
 impl GameMaster {
-    pub fn initialize_game(num_contestants: i32) -> GameMaster {
-        // Create n number of contestants, randomly pair off into perfect matches
+    pub fn initialize_game(num_contestants: i32, max_iterations: i32) -> GameMaster {
+        // Create n number of contestants, randomly pair off into perfect .matches
         let mut pairs: Vec<ContestantPair> = vec![];
 
         // Each contestant is assigned an ID
         for i in (0..num_contestants - 1).step_by(2) {
-            pairs.push(ContestantPair::new(Player::new(i), Player::new(i+1)));
+            pairs.push(ContestantPair::new(Player::new(i), Player::new(i + 1)));
         }
 
-        return GameMaster {iterations: 0, pairs};
+        return GameMaster {
+            iterations: 0,
+            matches: pairs,
+            num_matched: 0,
+            max_iterations
+        };
     }
     pub fn contestants(&self) -> Vec<&Player> {
         let mut players: Vec<&Player> = vec![];
 
-        for pair in self.pairs.iter() {
-            players.push(&pair.getA());
-            players.push(&pair.getB());
+        for pair in self.matches.iter() {
+            players.push(&pair.get_a());
+            players.push(&pair.get_b());
         }
         let mut rng = thread_rng();
         players.shuffle(&mut rng);
         return players;
     }
 
-    fn isPerfectMatch(&self, other_pair: &ContestantPair) -> bool {
-        for matched_pair in self.pairs.iter() {
-            if matched_pair == other_pair {
+    fn is_match(&self, pair: &ContestantPair) -> bool {
+        for matched_pair in self.matches.iter() {
+            if matched_pair == pair {
                 return true;
             }
         }
         return false;
     }
 
-}
+    pub fn make_guess(&mut self, guess: &ContestantPair) -> bool {
+        println!("ROUND {}-------", self.iterations);
+        println!("Attempted match {}", guess);
+        self.iterations += 1;
 
+        if self.is_match(&guess) {
+            println!("Guessed correctly!\n");
+            self.num_matched += 1;
+            return true;
+        } else {
+            println!("Wrong guess!\n");
+            return false;
+        }
+
+    }
+
+    pub fn output_stats(&self) {
+        println!("{} perfect matches were found successfully after {} iterations",
+            self.contestants().len(),
+            self.iterations
+        );
+    }
+
+    pub fn get_iterations(&self) -> i32 {
+        return self.iterations;
+    }
+
+    pub fn in_progress(&self) -> bool {
+        return (self.iterations < self.max_iterations) && (self.num_matched != i32::try_from(self.matches.len()).unwrap());
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::contestant::{ContestantPair, Player};
     use crate::gamemaster::GameMaster;
-    use crate::contestant::{Player, ContestantPair, ContestantPairs};
-
 
     #[test]
     fn test_game_initialized() {
-        let game = GameMaster::initialize_game(12);
-        assert_eq!(game.pairs.len(), 6);
+        let game = GameMaster::initialize_game(12, 10);
+        assert_eq!(game.matches.len(), 6);
         assert_eq!(game.contestants().len(), 12);
     }
 
     #[test]
     fn test_get_contestants() {
         let num_players = 12;
-        let game = GameMaster::initialize_game(num_players);
-        
+        let game = GameMaster::initialize_game(num_players, 10);
         // Check that the randomized contestant list is not the same as the one derived from matched pair order
         let randomized: Vec<&Player> = game.contestants();
         let mut num_equal = 0;
@@ -72,22 +105,59 @@ mod tests {
         }
         assert_ne!(num_equal, randomized.len());
 
-        // Check that the matched pairs are still in order after shuffles 
-        for i in 0..game.pairs.len() {
-            assert_eq!(game.pairs[i].getA().id as usize, 2*i);
-            assert_eq!(game.pairs[i].getB().id as usize, 2*i+1);
+        // Check that the matched pairs are still in order after shuffles
+        for i in 0..game.matches.len() {
+            assert_eq!(game.matches[i].get_a().id as usize, 2 * i);
+            assert_eq!(game.matches[i].get_b().id as usize, 2 * i + 1);
         }
     }
+
     #[test]
     fn test_is_perfect_match() {
         let p1 = Player::new(1);
         let p2 = Player::new(2);
         let matched = ContestantPair::new(p1, p2);
-        let game_master = GameMaster {iterations: 0, pairs: vec![matched]};
+        let game_master = GameMaster {
+            iterations: 0,
+            matches: vec![matched],
+            num_matched: 0,
+            max_iterations: 10
+        };
 
-        assert_eq!(game_master.isPerfectMatch(&ContestantPair::new(Player::new(1), Player::new(2))), true);
-        assert_eq!(game_master.isPerfectMatch(&ContestantPair::new(Player::new(2), Player::new(1))), true);
-        assert_eq!(game_master.isPerfectMatch(&ContestantPair::new(Player::new(3), Player::new(1))), false);
+        assert_eq!(
+            game_master.is_match(&ContestantPair::new(Player::new(1), Player::new(2))),
+            true
+        );
+        assert_eq!(
+            game_master.is_match(&ContestantPair::new(Player::new(2), Player::new(1))),
+            true
+        );
+        assert_eq!(
+            game_master.is_match(&ContestantPair::new(Player::new(3), Player::new(1))),
+            false
+        );
     }
 
+    #[test]
+    fn test_in_progress_found_matches() {
+        let mut game = GameMaster::initialize_game(12, 50);
+        assert_eq!(game.in_progress(), true);
+        for pair in game.matches.clone().iter() {
+            assert_eq!(game.make_guess(&pair), true);
+        }
+        assert_eq!(game.in_progress(), false);
+    }
+
+    #[test]
+    fn test_in_progress_exceeds_limit() {
+        let mut game = GameMaster::initialize_game(12, 5);
+        assert_eq!(game.in_progress(), true);
+
+        let cloned = game.matches.clone();
+        for i in 0..6 {
+            assert_eq!(game.make_guess(cloned.get(0).unwrap()), true);
+        }
+        
+        assert_eq!(game.in_progress(), false);
+    }
 }
--- src/gamestrategy.rs
+++ src/gamestrategy.rs
@@ -1,3 +1,36 @@
+use crate::contestant::{Player, ContestantPair};
+
+
 pub trait GameStrategy {
-    
+    fn guess(&self) -> ContestantPair;
+    fn handle_feedback(&self, guessed_right: bool);
+}
+
+pub struct SimpleStrategy {
+    contestants: Vec<Player>
+}
+impl SimpleStrategy {
+    pub fn initialize(contestants: Vec<&Player>) -> SimpleStrategy {
+        let mut cloned_contestants: Vec<Player> = vec![];
+        
+        for player in contestants.iter() {
+            cloned_contestants.push((*player).clone());
+        }
+
+        SimpleStrategy { contestants: cloned_contestants }
+    }
+
+}
+impl GameStrategy for SimpleStrategy {
+
+    fn guess(&self) -> ContestantPair {
+        let player_a = self.contestants.get(0).unwrap();
+        let player_b = self.contestants.get(1).unwrap();
+        
+        ContestantPair::new(player_a.clone(), player_b.clone())
+    }
+
+    fn handle_feedback(&self, guessed_right: bool) {
+        
+    }
 }
