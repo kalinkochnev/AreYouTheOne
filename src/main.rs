d8b8b98ca8e0698d22e56f7700d24dde2ed33ce1
kalinkochnev <kalin.kochnev@gmail.com>
2022-04-04 17:05:32 -0400

    Added main game procedure

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ pub mod gamemaster;
 pub mod gamestrategy;
 
 use crate::gamemaster::GameMaster;
+use crate::gamestrategy::{GameStrategy, SimpleStrategy};
 
 fn main() {
-    // let game = GameMaster::initializeGame(12);
+    let mut game = GameMaster::initialize_game(12, 100);
+    let strategy: Box<dyn GameStrategy> = Box::new(SimpleStrategy::initialize(game.contestants()));
+
+    while game.in_progress() {
+        let was_correct = game.make_guess(&strategy.guess());
+        strategy.handle_feedback(was_correct);
+    }
+    game.output_stats();
 }
