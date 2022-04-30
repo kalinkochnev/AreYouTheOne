df946288723cfca5e34b6d9cd2313253c7c75c85
kalinkochnev <kalin.kochnev@gmail.com>
2022-04-30 14:26:37 -0400

    Basic possible_pairing test passes!

--- src/bruteforce.rs
+++ src/bruteforce.rs
@@ -1,3 +1,5 @@
+use crate::contestant::ContestantPairs;
+use crate::utils::pairs_to_contestants;
 use crate::roundmanager::RoundManager;
 use std::collections::HashSet;
 use std::collections::HashMap;
@@ -83,6 +85,7 @@ impl BruteForce {
     fn pair_player(&self, player: &Player, off_limits: &HashSet<Player>) -> PairPlayerResult {
         // Given a player, try to get a player who isn't already picked. 
         let player_poss = self.possibilities.get(player).expect("Player not in possibilities");
+
         let mut can_pair_with = player_poss.difference(off_limits);
 
         // If that is not possible, return a contradiction
@@ -92,26 +95,98 @@ impl BruteForce {
         };
     }
 
-    fn choose_ceremony(&self) {
-        todo!();
+    /*This finds a player in the current set of possibilities that has the fewest
+    number of possibilities left.*/
+    fn highest_prob_player(&self, filtered: &HashSet<Player>) -> Option<Player> {
+        let mut best_player: Option<(&Player, usize)> = None;
+        for (player_key, poss_pairs) in self.possibilities.iter() {
+            if filtered.contains(player_key) {
+                continue;
+            }
 
-        // If we have any rounds available, choose the one that is the most likely and take half of the possibile pairs for additional checking
-        let ceremony_guess = Vec::new();
-
-        if self.round_manager.should_use_round(&self.possibilities) {
-            let best_round = self.round_manager.most_eff().unwrap();
-            let num_pairs = self.contestants.len() / 2;
-            ceremony_guess.push(best_round.pick_from_round(num_pairs / 2));
+            match best_player {
+                Some(best) => {
+                    if poss_pairs.len() < best.1 {
+                        best_player = Some((player_key, poss_pairs.len()))
+                    }
+                }
+                None => {
+                    best_player = Some((player_key, poss_pairs.len()))
+                }
+            }
         }
 
-        // We need to keep choosing until we find a set of pairs that does not contradict itself
-        let mut num_contradictions = 0;
-        
-        while ceremony_guess.len() < self.contestants.len() / 2 {
+        match best_player {
+            Some((player, count)) => return Some(player.clone()),
+            None => return None,
+        }
+    }
+
+    fn filter_possibilities(possible_pairs: &HashSet<Player>, off_limits: &HashSet<Player>) -> HashSet<Player> {
+        let difference: HashSet<&Player> = possible_pairs.difference(&off_limits).collect();
+        return difference.into_iter().map(|item| item.to_owned()).collect();
+    }
+
+    /* This does an initial depth first search for a pair, and if it is 
+    not possible to form a pair with the given combination, then it
+    works backwards from the contraditing pair to find said pair */
+    fn possible_pairing(&self) -> Vec<ContestantPair>{
+        // Poss stack keeps track of players and their possibilities (which are filtered as you go down the stack)
+        // Current pairing is the actual pairing 
+        let mut poss_stack: Vec<(Player, HashSet<Player>)> = Vec::new();
+        let mut current_pairing: Vec<ContestantPair> = Vec::new();
+
+        { // Start with the highest likelihood pair
+            let initial_player = self.highest_prob_player(&HashSet::new()).unwrap();
+            poss_stack.push((
+                initial_player.clone(), 
+                self.possibilities.get(&initial_player).unwrap().clone()
+            ));
+        }
+
+        // keep going until you have found a full set of current_pairs
+        while current_pairing.len() < self.contestants.len() / 2 {
+            println!("Poss {}", ContestantPairs(&current_pairing));
+            // from the top of the stack, add the next stack of possibilities
+            let (player_a, player_a_poss) = poss_stack.last_mut().unwrap();
+            let player_a_match = player_a_poss.iter().next().cloned();
             
-        }
+            if let Some(player_b) = player_a_match {
+                // add to the current pairing
+                player_a_poss.remove(&player_b);
+                current_pairing.push(ContestantPair::new(player_a.clone(), player_b.clone()));
 
-        
+                // create the next stack of possibilities
+                let off_limits: HashSet<Player> = pairs_to_contestants(&current_pairing).into_iter().collect();
+                match self.highest_prob_player(&off_limits) {
+                    // If there is a player with options left, add it to the stack
+                    Some(new_player) => {
+                        let mut player_possibilities = self.possibilities.get(&new_player).unwrap().clone();
+                        player_possibilities = BruteForce::filter_possibilities(&player_possibilities, &off_limits);
+
+                        // add the new player and it's possible pairings to the stack
+                        poss_stack.push((new_player, player_possibilities));
+                    },
+
+                    // Otherwise, do nothing. The next iteration of the loop will either exit
+                    // bc all matches were found or will try different combinations
+                    None => {}
+                };
+
+            
+            } else {
+                // If there is no match, that means there are no possibilities left, means there is a contradition
+                // Keep deleting stacks as long as they are empty (and the current pairing array to remove bad pairs)
+                let mut new_stack = poss_stack.last_mut().unwrap();
+                while new_stack.1.len() == 0 { // new_stack.1 is the possibilities
+                    poss_stack.pop().unwrap();
+                    current_pairing.pop();
+                    
+                    new_stack = poss_stack.last_mut().unwrap();
+                }
+            }
+        }
+        return current_pairing;
     }
 
     fn handle_correct_match(&self, pair: &ContestantPair) {
@@ -278,15 +353,30 @@ mod tests {
     }
 
     #[test]
+    fn test_highest_prob_player() {
+        let c = gen_contestants(4);
+        let mut strategy = BruteForce::initialize(c.iter().collect());
+
+        strategy.possibilities = HashMap::from([
+            (c[0].clone(), vec![              c[1].clone(), c[2].clone(), c[3].clone()].into_iter().collect()),
+            (c[1].clone(), vec![c[0].clone(),               c[2].clone(), c[3].clone()].into_iter().collect()),
+            (c[2].clone(), vec![c[0].clone()  /*c[1],   c[3]*/                        ].into_iter().collect()), // We create a contradiction by letting only c[2] be paired w/ c[0], but our off limits makes it so there's no one to pair with
+            (c[3].clone(), vec![c[0].clone(), c[1].clone(), c[2].clone(),             ].into_iter().collect()),
+        ]);
+            
+        assert_eq!(strategy.highest_prob_player(&HashSet::new()).unwrap(), c[2].clone());
+    }
+
+    #[test]
     fn test_get_no_contradiction_ceremony() {
         let c = gen_contestants(4);
         let mut strategy = BruteForce::initialize(c.iter().collect());
 
         strategy.possibilities = HashMap::from([
-            (c[0].clone(), vec![      c[1].clone(), c[2].clone(), c[3].clone()].into_iter().collect()),
-            (c[1].clone(), vec![c[0].clone(),       c[2].clone(), c[3].clone()].into_iter().collect()),
-            (c[2].clone(), vec![c[0].clone()  /*c[1],   c[3]*/].into_iter().collect()), // We create a contradiction by letting only c[2] be paired w/ c[0], but our off limits makes it so there's no one to pair with
-            (c[3].clone(), vec![c[0].clone(), c[1].clone(), c[2].clone(),     ].into_iter().collect()),
+            (c[0].clone(), vec![              c[1].clone(), c[2].clone(), c[3].clone()].into_iter().collect()),
+            (c[1].clone(), vec![c[0].clone(),               c[2].clone(), c[3].clone()].into_iter().collect()),
+            (c[2].clone(), vec![c[0].clone()  /*c[1],   c[3]*/                        ].into_iter().collect()), // We create a contradiction by letting only c[2] be paired w/ c[0], but our off limits makes it so there's no one to pair with
+            (c[3].clone(), vec![c[0].clone(), c[1].clone(), c[2].clone(),             ].into_iter().collect()),
         ]);
         
         // Only combo that works is [(c[2], c[0]), (c[1], c[3])], however we can create a contradiction
@@ -295,7 +385,7 @@ mod tests {
         assert_eq!(PairPlayerResult::Contradiction, strategy.pair_player(&c[2], &off_limits));
 
         let expected_pair = ContestantPair::new(c[2].clone(), c[0].clone());
-        assert_eq!(&expected_pair, strategy.ceremony_pairs().get(0).unwrap());
+        assert_eq!(&expected_pair, strategy.possible_pairing().get(0).unwrap());
     }
 
 
